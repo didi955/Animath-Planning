387a03b9b07710f8fa39865380beb6016368542f
marks <maxime.caillaud93@gmail.com>
2022-12-06 10:57:53 +0100

    Update Views (view_home,view_topbar) Add View (view_footer)

---
+++ src/Views/view_footer.php
@@ -0,0 +1,9 @@
+
+<div class="w-100 bg-dark border-top border-dark border-3 shadow-lg text-center" style="height: 10%">
+    <nav class="row nav text-center position-relative p-4">
+        <a class="col nav-link" aria-current="page" href="#">Support</a>
+        <a class="col nav-link" href="#">FAQ</a>
+        <a class="col nav-link" href="https://www.animath.fr/a-propos/mission/">A propos</a>
+        <a class="col nav-link">CGU</a>
+    </nav>
+</div>
--- src/Views/view_home.php
+++ src/Views/view_home.php
@@ -3,4 +3,31 @@ require "Layouts/header.php";
 require "view_topbar.php";
 ?>
 
+<img class= "w-100 h-100" src="public/images/logo.jpg" alt="logo">
+
+
+
+<div class="container text-center">
+    <div class="row w-100 m-5" style="height: 20%">
+        <div class="col-md-9 h-100 pt-5"> Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet,</div>
+        <div class="col-md-3 h-100 bg-light"> Image </div>
+    </div>
+    <div class="row w-100 m-5" style="height: 20%">
+        <div class="col-md-3 h-100 bg-light"> Image </div>
+        <div class="col-md-9 h-100 pt-5">Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet,</div>
+    </div>
+    <div class="row w-100 m-5" style="height: 20%">
+        <div class="col-md-9 h-100 pt-5">Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet,</div>
+        <div class="col-md-3 h-100 bg-light"> Image </div>
+    </div>
+    <div class="row w-100 m-5" style="height: 20%">
+        <div class="col-md-3 h-100 bg-light"> Image </div>
+        <div class="col-md-9 h-100 pt-5">Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, </div>
+    </div>
+</div>
+
+<?php
+require "view_footer.php";
+?>
+
 
