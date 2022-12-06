30db6eb2bfdcb6e0385192cd3865f457faecd19b
ABED Rayane <12102152@crit-xubuntu>
2022-12-06 11:19:43 +0100

    update

---
+++ src/Views/view_legal.php
@@ -0,0 +1,13 @@
+<div class="modal fade" id="cguModal" tabindex="-1" aria-labelledby="cguModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">CGU</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="overflow-auto">LOREM IPSUM DOLAR AMTER LADADJAIDHJADKJHADAA</div>
+            </div>
+        </div>
+    </div>
+</div>
