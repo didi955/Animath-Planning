68413d82bbd3f0e221f98d6b388234c776ce26a5
marks <maxime.caillaud93@gmail.com>
2022-12-08 15:23:11 +0100

    Update background

--- src/Layouts/header.php
+++ src/Layouts/header.php
@@ -5,5 +5,5 @@
 </head>
 <script src='https://www.hCaptcha.com/1/api.js' async defer></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-<body style="overflow-x: hidden;background-image:url(&#34;public/images/logo.jpg&#34;);background-attachment:fixed">
+<body style="overflow-x: hidden;background-image:url(&#34;public/images/Animath.jpg&#34;);background-attachment:fixed">
 
