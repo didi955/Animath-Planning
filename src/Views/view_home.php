2de75013d2084457f807b24e2b58a0df17b428d3
marks <maxime.caillaud93@gmail.com>
2022-12-08 12:24:36 +0100

    Change view Home / Layout header (adding connexion et inscription button + background fixed)
    Fixed top bar
    
    Fixed footer color and style

--- src/Views/view_home.php
+++ src/Views/view_home.php
@@ -2,28 +2,103 @@
 include "view_topbar.php";
 ?>
 
-<img class= "w-100 h-100" src="public/images/logo.jpg" alt="logo">
+<div class="w-100" style="height: 93%"></div>
 
 
-
-<div class="container text-center">
-    <div class="row w-100 m-5" style="height: 20%">
-        <div class="col-md-9 h-100 pt-5"> Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet,</div>
-        <div class="col-md-3 h-100 bg-light"> Image </div>
-    </div>
-    <div class="row w-100 m-5" style="height: 20%">
-        <div class="col-md-3 h-100 bg-light"> Image </div>
-        <div class="col-md-9 h-100 pt-5">Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet,</div>
-    </div>
-    <div class="row w-100 m-5" style="height: 20%">
-        <div class="col-md-9 h-100 pt-5">Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet,</div>
-        <div class="col-md-3 h-100 bg-light"> Image </div>
-    </div>
-    <div class="row w-100 m-5" style="height: 20%">
-        <div class="col-md-3 h-100 bg-light"> Image </div>
-        <div class="col-md-9 h-100 pt-5">Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, </div>
+<div class="card bg-light" style="height: 7%">
+    <div class="row text-center fs-5 h-100">
+       <span class="col-6 border-end pt-3">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Inscription</button>
+        </span>
+        <span class="col-6 border-start pt-3">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne">Inscription</button>
+        </span>
     </div>
 </div>
+
+<div class="bg-light accordion-item" id="collapseOne">
+    <form class="container" action="?controller=User&action=sign_in" method="post">
+        <div class="mb-3">
+            <label class="col form-label" for="login">Identifiant/Mail :</label><input class="col form-control" type="email" id="email" name="email" required>
+        </div>
+        <div class="mb-3">
+            <label class="col form-label" for="password">Mot de passe : </label><input class="col form-control" type="password" id="pass" name="pass" required>
+        </div>
+        <div class="modal-footer">
+            <input type="submit" class="btn btn-primary" value="Se connecter"/>
+        </div>
+    </form>
+</div>
+
+<div class="bg-light accordion-item" id="collapseTwo">
+    <form class="container needs-validation" method="post" action="?controller=User&action=sign_up" novalidate>
+        <div class="mb-3">
+            <label class="form-label" for="email">Mail :</label>
+            <input class="form-control" type="email" id="email" name="email" required>
+            <div class="invalid-feedback">Veuillez entrer une adresse mail valide.</div>
+            <div class="valid-feedback">Adresse mail valide.</div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="first_name">Prénom :</label>
+            <input class="form-control" type="text" id="first_name" name="first_name" required>
+            <div class="invalid-feedback">Veuillez entrer un prénom valide.</div>
+            <div class="valid-feedback">Prénom valide.</div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="last_name">Nom :</label>
+            <input class="form-control" type="text" id="last_name" name="last_name" required>
+            <div class="invalid-feedback">Veuillez entrer un nom valide.</div>
+            <div class="valid-feedback">Nom valide.</div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="pass">Mot de passe : </label>
+            <input class="form-control" type="password" id="pass" name="pass" required>
+            <div class="invalid-feedback">Veuillez entrer un mot de passe valide.</div>
+            <div class="valid-feedback">Mot de passe valide.</div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="pass_confirm">Confirmation mot de passe : </label>
+            <input class="form-control" type="password" id="pass_confirm" name="pass_confirm" required>
+            <div class="invalid-feedback">Veuillez entrer un mot de passe valide.</div>
+            <div class="valid-feedback">Mot de passe valide.</div>
+        </div>
+        <div class="mb-3">
+            <input type="checkbox" name="cgu_accept" id="cgu" class="form-check-input"/>
+            <label for="cgu">J'accepte les <a data-bs-toggle="modal" href="#cguModalTopbar"><?=e(CGU_TITLE)?></a></label>
+            <div class="invalid-feedback">Veuillez accepter les CGU, pour continuer</div>
+        </div>
+        <?php
+        /*
+        $key = parse_ini_file("hcaptcha.ini")['key'];
+        echo '<div class="h-captcha" data-sitekey="'. $key .'"></div>';
+        */
+        ?>
+        <div class="modal-footer">
+            <input type="submit" id="submit_signup" class="btn btn-primary"/>
+        </div>
+    </form>
+</div>
+
+
+
+    <div class="container border shadow ps-5 pt-5 pb-5 text-center bg-white">
+        <div class="row w-100 mb-2" style="height: 20%">
+            <div class="col-md-9 h-100 pt-5"> Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet,</div>
+            <div class="col-md-3 h-100 bg-light"> Image </div>
+        </div>
+        <div class="row w-100 mb-2" style="height: 20%">
+            <div class="col-md-3 h-100 bg-light"> Image </div>
+            <div class="col-md-9 h-100 pt-5">Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet,</div>
+        </div>
+        <div class="row w-100 mb-2" style="height: 20%">
+            <div class="col-md-9 h-100 pt-5">Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet,</div>
+            <div class="col-md-3 h-100 bg-light"> Image </div>
+        </div>
+        <div class="row w-100 mb-2" style="height: 20%">
+            <div class="col-md-3 h-100 bg-light"> Image </div>
+            <div class="col-md-9 h-100 pt-5">Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, Lorem Ispum dolor sit amet, </div>
+        </div>
+    </div>
 
 <?php
 include "view_footer.php";
--- src/Views/view_topbar.php
+++ src/Views/view_topbar.php
@@ -1,52 +1,54 @@
-<div class="row align-items-center fixed-top position-absolute mt-2" style="width: 15%;left:85%;height: 10%" >
-    <span class="col w-50 form-check-reverse form-switch text-center">
-            <label for="darkMode"></label><input class="mx-1 form-check-input " type="checkbox" id="darkMode" role="switch">
-    </span>
-    <span class="col w-50">
-        <img class="w-75 btn" data-bs-toggle="collapse" data-bs-target="#collapseMenu" role="button" aria-expanded="true" aria-controls="collapseMenu" src="public/images/avatar.png" alt="mon_compte">
-    </span>
-    <div class="collapse" id="collapseMenu">
-        <div class="card card-body">
-            <?php if(isset($_SESSION['uuid'])): ?>
-                <a class="btn btn-primary m-1" href="?controller=User&action=my_account">Mon compte</a>
-                <a class="btn btn-primary m-1" href="?controller=User&action=sign_out">Se déconnecter</a>
-            <?php elseif(isset($_COOKIE['uuid'])): ?>
-                <a class="btn btn-primary m-1" href="?controller=User&action=my_account">Mon compte</a>
-                <a class="btn btn-primary m-1" href="?controller=User&action=sign_out">Se déconnecter</a>
-            <?php else : ?>
-                <p class="text-center">Vous n'êtes pas connecté !</p>
-                <span class="row">
-                <button class="col border-right btn btn-light border m-1" data-bs-toggle="modal" href="#connexionModal">Connexion</button>
-                <button class="col border-left btn btn-primary border m-1" data-bs-toggle="modal" href="#inscriptionModal">Inscription</button>
-            <?php endif; ?>
-        </span>
+    <div class="fixed-top">
+        <div class="row align-items-center position-absolute mt-2" style="width: 15%;left:85%;height: 10%" >
+            <span class="col w-50 form-check-reverse form-switch text-center">
+                <label for="darkMode"></label><input class="mx-1 form-check-input " type="checkbox" id="darkMode" role="switch">
+            </span>
+            <span class="col w-50">
+                <img class="w-75 btn" data-bs-toggle="collapse" data-bs-target="#collapseMenu" role="button" aria-expanded="true" aria-controls="collapseMenu" src="public/images/avatar.png" alt="mon_compte">
+            </span>
+            <div class="collapse" id="collapseMenu">
+                <div class="card card-body">
+                    <?php if(isset($_SESSION['uuid'])): ?>
+                    <a class="btn btn-primary m-1" href="?controller=User&action=my_account">Mon compte</a>
+                    <a class="btn btn-primary m-1" href="?controller=User&action=sign_out">Se déconnecter</a>
+                    <?php elseif(isset($_COOKIE['uuid'])): ?>
+                    <a class="btn btn-primary m-1" href="?controller=User&action=my_account">Mon compte</a>
+                    <a class="btn btn-primary m-1" href="?controller=User&action=sign_out">Se déconnecter</a>
+                    <?php else : ?>
+                    <p class="text-center">Vous n'êtes pas connecté !</p>
+                    <span class="row">
+                        <button class="col border-right btn btn-light border m-1" data-bs-toggle="modal" href="#connexionModal">Connexion</button>
+                        <button class="col border-left btn btn-primary border m-1" data-bs-toggle="modal" href="#inscriptionModal">Inscription</button>
+                        <?php endif; ?>
+                    </span>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 
-<div class="modal fade" id="connexionModal" tabindex="-1" aria-labelledby="connexionModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Connexion</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form class="container" action="?controller=User&action=sign_in" method="post">
-                    <div class="mb-3">
-                        <label class="col form-label" for="login">Identifiant/Mail :</label><input class="col form-control" type="email" id="email" name="email" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="col form-label" for="password">Mot de passe : </label><input class="col form-control" type="password" id="pass" name="pass" required>
-                    </div>
-                    <div class="modal-footer">
+    <div class="modal fade" id="connexionModal" tabindex="-1" aria-labelledby="connexionModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Connexion</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form class="container" action="?controller=User&action=sign_in" method="post">
+                        <div class="mb-3">
+                            <label class="col form-label" for="login">Identifiant/Mail :</label><input class="col form-control" type="email" id="email" name="email" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="col form-label" for="password">Mot de passe : </label><input class="col form-control" type="password" id="pass" name="pass" required>
+                        </div>
+                        <div class="modal-footer">
                         <input type="submit" class="btn btn-primary" value="Se connecter"/>
-                    </div>
-                </form>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 <div class="modal fade" id="inscriptionModal" tabindex="-1" aria-labelledby="inscriptionModalLabel" aria-hidden="true">
     <div class="modal-dialog">
