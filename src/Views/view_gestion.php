45911c08f599c733c947719d7ce167631e527849
marks <maxime.caillaud93@gmail.com>
2023-01-05 11:19:29 +0100

    Few changes

--- src/Views/view_gestion.php
+++ src/Views/view_gestion.php
@@ -1,20 +1,37 @@
 <?php
 include_once "view_topbar.php";
 ?>
+<style>
+    body {
+        background: url("public/images/AnimathBlur.png") no-repeat fixed center center;
+    }
+</style>
+<script src="public/js/fullcalendar-6.0.0/dist/index.global.js">
+</script>
 
-
-<div class="w-100" style="height: 10%"></div>
-<h1 class="text-center fw-bold text-decoration-underline">Gestion</h1>
-
-<div class="w-100" style="height: 3%"></div>
-
+<div class="border-bottom mt-3 mb-3">
+    <div class="container pt-3 pb-3 text-center">
+        <div class="row">
+            <div class="col-12">
+                <h1>Gestion</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="border">
 <?php
-require_once "Layouts/supervisor/gestion.php";
+$req = DatabaseModel::getModel()->getBD()->prepare('select * from stand') ;
+$req->execute();
+$stands = $req->fetchAll(PDO::FETCH_ASSOC);
+foreach ($stands as $stand) {
+    echo "<div class='card'><div class='card-header'>$stand[title]</div><div class>";
+    foreach ($stand as $key => $value) {
+        echo "<div>$key: $value</div>";
+    }
+    echo "</div><div class='card-footer'></div></div>";
+}
 ?>
-
-<div class="w-100" style="height: 52%"></div>
-
-
+</div>
 <?php
 include_once "view_footer.php";
 ?>
