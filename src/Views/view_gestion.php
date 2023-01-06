e00b118b92ec3138e7d0e1c7abe29adcd06d507d
marks <maxime.caillaud93@gmail.com>
2023-01-06 12:02:26 +0100

    Front End Gestion des activités

--- src/Views/view_gestion.php
+++ src/Views/view_gestion.php
@@ -29,7 +29,17 @@ $i = 0?>
 <div class="ps-5 container">
     <div class="ps-5 ms-5 row gap-5">
         <?php foreach ($stands as $stand):?>
-            <div class="card col-3" style="height: 15rem">
+            <div class="modal fade" id="<?php echo e("standCalendar$stand[id]") ?>" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-body" style="height: 50rem">
+                            <div class="h-75" id="calendar<?php echo e("$stand[id]") ?>">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card shadow col-3" style="height: 15rem">
                 <div class="card-header p-3">
                     <?php echo e("$stand[title]") ?>
                 </div>
@@ -40,17 +50,24 @@ $i = 0?>
                     <a class="btn btn-primary" href="<?php echo e("#standCalendar$stand[id]")?>" data-bs-toggle="modal">Gerer le planing</a>
                 </div>
             </div>
-            <div class="modal fade" id="<?php echo e("standCalendar$stand[id]") ?>" tabindex="-1" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            <div id="calendar">
-                                <?php echo e("standCalendar$stand[id]") ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    let calendarEl = document.getElementById('calendar<?php echo e("$stand[id]") ?>');
+                    let calendar = new FullCalendar.Calendar(calendarEl, {
+                        initialView: 'timeGridDay',
+                        locale: 'fr',
+                        editable: true,
+                        allDaySlot: false,
+                        events: [
+                            {
+                                title: '<?php echo e("$stand[title]") ?>',
+                                start: '2023-01-06T15:00:00'
+                            }
+                        ]
+                    });
+                    calendar.render();
+                });
+            </script>
         <?php endforeach; ?>
     </div>
 </div>
