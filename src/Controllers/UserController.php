7784dd84faf173f903f1e1175c0224e67afe403f
Dylan Lannuzel <lannuzeld@gmail.com>
2023-01-08 22:04:17 +0100

    some updates

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -112,19 +112,22 @@ class UserController extends Controller
     public function action_sign_up(){
         if(isset($_POST['email'])){
             $mail = strtolower($_POST['email']);
-            if(isset($_POST['h-captcha-response'])){
-                if(is_valid_email($mail)
-                    && isset($_POST['pass']) && isset($_POST['pass_confirm']) && $_POST['pass'] === $_POST['pass_confirm'] &&
-                    isset($_POST['last_name']) && isset($_POST['first_name']) && is_valid_name($_POST['last_name']) && is_valid_name($_POST['first_name']) &&isset($_POST['cgu_accept']) && $_POST['cgu_accept'] === 'on'){
+            if(is_valid_email($mail)
+                && isset($_POST['pass']) && isset($_POST['pass_confirm']) && $_POST['pass'] === $_POST['pass_confirm'] &&
+                isset($_POST['last_name']) && isset($_POST['first_name']) && is_valid_name($_POST['last_name']) && is_valid_name($_POST['first_name']) &&isset($_POST['cgu_accept']) && $_POST['cgu_accept'] === 'on'){
 
+                if(CAPTCHA_ENABLED){
+                    if(!isset($_POST['h-captcha-response'])){
+                        $this->action_error('Captcha invalide', 444);
+                        return;
+                    }
                     $secret = parse_ini_file("../hcaptcha.ini")['secret'];
                     $data = array(
                         'response' => $_POST['h-captcha-response'],
                         'secret' => $secret
                     );
                     // POST REQUEST TO CHECK THE VALIDITY OF THE CAPTCHA TOKEN
-                    //NOT WORK, NEED TO BE FIXED
-                    /*
+
                     $verify = curl_init();
                     curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
                     curl_setopt($verify, CURLOPT_POST, true);
@@ -134,49 +137,44 @@ class UserController extends Controller
                     $responseData = json_decode($response);
 
                     if($responseData->success) {
-                        if(UserModel::getModel()->getUserByEmail($mail) == null) {
-                            $user = new User();
-                            $user->setUUID(Uuid::uuid4()->toString());
-                            $user->setEmail($mail);
-                            $user->setActive(false);
-                            $user->setPassHash(hash_pass($_POST['pass']));
-                            $user->setLastName($_POST['last_name']);
-                            $user->setFirstName($_POST['first_name']);
-                            $user->setRole(Role::PROFESSOR);
-                            $user->save();
-                            $_SESSION['user'] = serialize($user);
-                            $this->render("home");
+                        if (UserModel::getModel()->getUserByConnexionID($mail) == null) {
+                            $this->initUser($mail);
                         }
-                    */
-                    if(UserModel::getModel()->getUserByConnexionID($mail) == null) {
-                        $user = new User();
-                        $user->setID(Uuid::uuid4()->toString());
-                        $user->setConnexionID($mail);
-                        $user->setActive(true);
-                        $user->setPassHash(hash_pass($_POST['pass']));
-                        $user->setRole(Role::PROFESSOR);
-                        $lastName = $_POST['last_name'];
-                        $firstName = $_POST['first_name'];
-                        $pdata = ['last_name' => $lastName, 'first_name' => $firstName, 'email' => $mail, 'phone' => null, 'school' => null];
-                        $user->setPersonalData($pdata);
-                        $user->save();
-                        $_SESSION['user'] = serialize($user);
-                        $this->render("home");
                     }
                     else {
-                        $this->action_error("Adresse mail déjà utilisée", 444);
+                        $this->action_error('Erreur dans la vérification du Captcha', 444);
                     }
                 }
+                elseif(UserModel::getModel()->getUserByConnexionID($mail) == null) {
+                    $this->initUser($mail);
+                }
                 else {
-                    $this->action_error('Formulaire invalide', 444);
+                    $this->action_error("Adresse mail déjà utilisée", 444);
                 }
             }
             else {
-                $this->action_error('Captcha invalide', 444);
+                $this->action_error('Formulaire invalide', 444);
             }
         }
         else {
             $this->action_error("Adresse mail non spécifiée", 444);
         }
     }
+
+    private function initUser($mail){
+        $user = new User();
+        $user->setID(Uuid::uuid4()->toString());
+        $user->setConnexionID($mail);
+        $user->setActive(true);
+        $user->setPassHash(hash_pass($_POST['pass']));
+        $user->setRole(Role::PROFESSOR);
+        $lastName = $_POST['last_name'];
+        $firstName = $_POST['first_name'];
+        $pdata = ['last_name' => $lastName, 'first_name' => $firstName, 'email' => $mail, 'phone' => null, 'school' => null];
+        $user->setPersonalData($pdata);
+        $user->save();
+        $_SESSION['user'] = serialize($user);
+        $this->render("home");
+    }
+
 }
