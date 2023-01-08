3e86e86d70eee984a669bb11025ad773c38025a3
marks <maxime.caillaud93@gmail.com>
2023-01-08 18:37:07 +0100

    Quelques ajouts

--- src/Views/view_gestion.php
+++ src/Views/view_gestion.php
@@ -26,12 +26,15 @@ $stands = $req?>
 </div>
 <div class="ps-5 container">
     <div class="ps-5 ms-5 row gap-5">
-        <?php foreach ($stands as $stand):?>
-            <div class="modal fade" id="<?php echo e("standCalendar$stand[id]") ?>" tabindex="-1" aria-hidden="true">
+        <?php foreach ($stands as $stand):
+            $id = $stand->getId();
+            $title = $stand->getTitle();
+            $desc = $stand->getDesc();?>
+            <div class="modal fade" id="<?php echo e("standCalendar$id") ?>" tabindex="-1" aria-hidden="true">
                 <div class="modal-dialog modal-xl">
                     <div class="modal-content">
                         <div class="modal-body" style="height: 50rem">
-                            <div class="h-75" id="calendar<?php echo e("$stand[id]") ?>">
+                            <div class="h-75" id="calendar<?php echo e("$id") ?>">
                             </div>
                         </div>
                     </div>
@@ -39,18 +42,18 @@ $stands = $req?>
             </div>
             <div class="card shadow col-3" style="height: 15rem">
                 <div class="card-header p-3">
-                    <?php echo e("$stand[title]") ?>
+                    <?php echo e("$title") ?>
                 </div>
                 <div class="card-body overflow-scroll p-3">
-                    <?php echo e("$stand[desc]"); ?>
+                    <?php echo e("$desc"); ?>
                 </div>
                 <div class="card-footer">
-                    <a class="btn btn-primary" href="<?php echo e("#standCalendar$stand[id]")?>" data-bs-toggle="modal">Gerer le planing</a>
+                    <a class="btn btn-primary" href="<?php echo e("#standCalendar$id")?>" data-bs-toggle="modal">Gerer le planing</a>
                 </div>
             </div>
             <script>
                 document.addEventListener('DOMContentLoaded', function() {
-                    let calendarEl = document.getElementById('calendar<?php echo e("$stand[id]") ?>');
+                    let calendarEl = document.getElementById('calendar<?php echo e("$id") ?>');
                     let calendar = new FullCalendar.Calendar(calendarEl, {
                         initialView: 'timeGridDay',
                         locale: 'fr',
@@ -58,7 +61,7 @@ $stands = $req?>
                         allDaySlot: false,
                         events: [
                             {
-                                title: '<?php echo e("$stand[title]") ?>',
+                                title: '<?php echo e("$id") ?>',
                                 start: '2023-01-06T15:00:00'
                             }
                         ]
