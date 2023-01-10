728158094fbd2f469dd08ab6063d60d534e28433
sxfiene <sofienehayek@gmail.com>
2023-01-10 22:03:06 +0100

    ajout confirmez mdp modifierProf.php

--- src/Layouts/modifierProf.php
+++ src/Layouts/modifierProf.php
@@ -1,8 +1,5 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
 <!-- E-mail Modal -->
-<div class="modal fade" id="emailModal" tabindex="-1" aria-labelledby="emailModalLabel" aria-hidden="true">
+<div class="modal fade modal-lg" id="emailModal" tabindex="-1" aria-labelledby="emailModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -24,7 +21,7 @@
 </div>
 
 <!-- Nom Modal -->
-<div class="modal fade" id="nomModal" tabindex="-1" aria-labelledby="nomModalLabel" aria-hidden="true">
+<div class="modal fade modal-lg" id="nomModal" tabindex="-1" aria-labelledby="nomModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -46,7 +43,7 @@
 </div>
 
 <!-- Prenom Modal -->
-<div class="modal fade" id="prenomModal" tabindex="-1" aria-labelledby="prenomModalLabel" aria-hidden="true">
+<div class="modal fade modal-lg" id="prenomModal" tabindex="-1" aria-labelledby="prenomModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -68,7 +65,7 @@
 </div>
 
 <!-- Telephone Modal -->
-<div class="modal fade" id="telephoneModal" tabindex="-1" aria-labelledby="telephoneModalLabel" aria-hidden="true">
+<div class="modal fade modal-lg" id="telephoneModal" tabindex="-1" aria-labelledby="telephoneModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -90,7 +87,7 @@
 </div>
 
 <!-- MDP Modal -->
-<div class="modal fade" id="MdpModal" tabindex="-1" aria-labelledby="MdpModalLabel" aria-hidden="true">
+<div class="modal fade modal-lg" id="MdpModal" tabindex="-1" aria-labelledby="MdpModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -106,6 +103,11 @@
 
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon1">Nouveau mot de passe</span>
+                    <input type="text" class="form-control" aria-describedby="basic-addon1">
+                </div>
+
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">Confirmez nouveau mot de passe</span>
                     <input type="text" class="form-control" aria-describedby="basic-addon1">
                 </div>
 
