63be5b554d0a83d3af3a06285e63e8616a00b55c
Dylan LANNUZEL <lannuzeld@gmail.com>
2023-01-12 11:24:42 +0100

    update

--- src/Utils/Role.php
+++ src/Utils/Role.php
@@ -3,15 +3,13 @@
 enum Role: int
 {
     case SUPERVISOR = 1;
-    case EXHIBITOR = 2;
-    case PROFESSOR = 3;
+    case PROFESSOR = 2;
 
     public static function valueOf(int $id): Role
     {
         return match ($id) {
             1 => Role::SUPERVISOR,
-            2 => Role::EXHIBITOR,
-            3 => Role::PROFESSOR,
+            2 => Role::PROFESSOR,
             default => throw new \InvalidArgumentException('Invalid role id'),
         };
     }
