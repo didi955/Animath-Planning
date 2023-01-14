2a3f4d1a84f30c0e6d4efc336705a7832dab4745
sxfiene <sofienehayek@gmail.com>
2023-01-14 16:21:42 +0100

    personnalData.php

--- src/Layouts/booking.php
+++ src/Layouts/booking.php
@@ -8,7 +8,7 @@
             <option value="l">Lycée</option>
         </select>
         <h2 class="pt-4 pb-3" style="text-align: left">Choissiez vos horaires pour la journée :</h2>
-        <label class="fs-3" style="text-align: left" for="meeting-time">Début :</label><input class="fs-4 w-100 h-30" type="datetime-local" id="meeting-time"
+        <label class="fs-3" style="justify-content: left" for="meeting-time">Début :</label><input class="fs-4 w-100 h-30" type="datetime-local" id="meeting-time"
                                                  name="meeting-time" value="2023-05-25T07:00"
                                                  min="2023-05-25T07:00" max="2023-05-26T19:00">
     </form>
