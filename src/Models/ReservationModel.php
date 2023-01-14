58748d9a4c5eb85c09d5f3ca0c3f7a61ea1442b3
Maxime <samir2003vlb@gmail.com>
2023-01-14 23:32:15 +0100

    Ajout Reservation.php et ReservationModel.php.

--- src/Models/ReservationModel.php
+++ src/Models/ReservationModel.php
@@ -1,4 +1,49 @@
 <?php
-class ReservationModel {
+
+class ReservationModel
+{
+    private static $instance = null;
+
+    private function __construct()
+    {
+    }
+
+    public static function getModel()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getReservation($id_user)
+    {
+        if (!$this->isInDatabase($id_user)) {
+            return null;
+        }
+        $req = DatabaseModel::getModel()->getBD()->prepare('SELECT * FROM "Appointement" WHERE id_user=:id_user');
+        $req->bindValue(":id_user", $id_user);
+        $req->execute();
+        $rs = $req->fetch(PDO::FETCH_ASSOC);
+        if (!$rs) {
+            return null;
+        }
+        $activities = ActivitiesModel::getModel()->getAllActivitiesByStand($id_user);
+        return $this->buildStand($rs, $activity);
+    }
+
+
+    public function isInDatabase($id_user,$id_activity)
+    {
+        $req = DatabaseModel::getModel()->getBD()->prepare('SELECT id FROM "Appointement" WHERE id_user= :id_user AND id_activity = :id_activity');
+        $req->bindValue(":id_user", $id_user);
+        $req->bindValue(":id_activity", $id_activity);
+        $req->execute();
+        $rs = $req->fetch();
+        if ($rs) {
+            return true;
+        }
+        return false;
+    }
 
 }
--- src/Reservation.php
+++ src/Reservation.php
@@ -2,5 +2,70 @@
 
 class Reservation
 {
-private int user
+
+    private $id_user ;
+
+
+    private $nb_student;
+
+    private $student_level;
+
+   private $activity;
+
+
+    public function __construct()
+    {
+
+    }
+
+    public function getIdUser()
+    {
+        return $this->id_user;
+    }
+
+    public function setIdUser($id_user): void
+    {
+        $this->id_user = $id_user;
+    }
+
+
+    public function getNbStudent()
+    {
+        return $this->nb_student;
+    }
+
+
+    public function setNbStudent($nb_student): void
+    {
+        $this->nb_student = $nb_student;
+    }
+
+
+    public function getStudentLevel()
+    {
+        return $this->student_level;
+    }
+
+
+    public function setStudentLevel($student_level): void
+    {
+        $this->student_level = $student_level;
+    }
+
+
+    public function getActivity()
+    {
+        return $this->activity;
+    }
+
+
+    public function setActivity($activity): void
+    {
+        $this->activity = $activity;
+    }
+
+
+
+
+
 }
