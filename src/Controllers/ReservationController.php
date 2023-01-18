1af2b3f9d3d4fada6c2665caca179d82eeb38871
Dylan LANNUZEL <lannuzeld@gmail.com>
2023-01-18 08:53:22 +0100

    update

--- src/Controllers/ReservationController.php
+++ src/Controllers/ReservationController.php
@@ -16,7 +16,7 @@ class ReservationController extends Controller
         if(isset($_POST['stud_level']) && isset($_POST['meeting-time_start']) && isset($_POST['meeting-time_end'])){
             $filtre = ['student_level' => $_POST["stud_level"], 'start' => $_POST["meeting-time_start"], 'end' => $_POST["meeting-time_end"]];
             $activities = ActivitiesModel::getModel()->getActivitiesWithFilter($filtre);
-            $this->render("myAccount",['user' => unserialize($_SESSION['user']), 'stand' => StandModel::getModel()->getAllStand(), 'activities' => $activities, 'student_level' => $_POST['stud_level']]);
+            $this->render("myAccount",['user' => unserialize($_SESSION['user']), 'stand' => StandModel::getModel()->getAllStand(), 'activities' => $activities, 'student_level' => $_POST['stud_level'],  $_SESSION['user']]);
         }
         else {
             $this->action_error("Veuillez remplir tous les champs");
@@ -27,7 +27,7 @@ class ReservationController extends Controller
         if(unserialize($_SESSION['user'])->getRole() === Role::PROFESSOR && isset($_POST['id_activity']) && isset($_POST['nb_student']) && isset($_POST['student_level'])){
             $activity_id = $_POST['id_activity'];
             ReservationModel::getModel()->addReservation(unserialize($_SESSION['user'])->getId(), $activity_id, $_POST['nb_student'], $_POST['student_level']);
-            $this->render("myAccount",['user' => unserialize($_SESSION['user']), 'stand' => StandModel::getModel()->getAllStand()]);
+            $this->render("myAccount",['user' => unserialize($_SESSION['user']), 'stand' => StandModel::getModel()->getAllStand(), 'appointements' => ReservationModel::getModel()->getMyReservations($_SESSION['user']->getID())]);
 
         }
     }
